2063cd882e90acb615da7a42c409719c8be0c97a
Cosmel VG <cosmelvg@gmail.com>
2021-03-31 13:36:32 -0600

    included function for currentime in order to render an h1 element if the time is greater the 12

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,9 +3,18 @@ import Login from "./Login";
 
 const IsLoggeIn = false;
 
+const curretTime = new Date().getHours();
+console.log(curretTime);
+
 function App() {
   return (
-    <div className="container">{IsLoggeIn ? <h1>Hello</h1> : <Login />}</div>
+    <div className="container">
+      {
+        // IsLoggeIn ? <h1>Hello</h1> : <Login />
+        // curretTime > 12 ? <h1>Why are you still working</h1> : null
+        curretTime > 12 && <h1>Why are you still working</h1>
+      }
+    </div>
   );
 }
 
